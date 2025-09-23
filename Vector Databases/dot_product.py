f27364dfa3b7a18358416cca10ccb5ad976d9045
Kavana Manvi Krishnamurthy <kavana.manvi@gmail.com>
2025-09-22 20:41:37 -0500

    Create dot_product.py

---
+++ Vector Databases/dot_product.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Define two vectors
+embedding1 = np.array([1.5, 2.0, 3.5])
+embedding2 = np.array([4.0, 1.0, 2.5])
+
+# Calculate the dot product
+dot_product = np.dot(embedding1, embedding2)
+print("Dot product:", dot_product)
